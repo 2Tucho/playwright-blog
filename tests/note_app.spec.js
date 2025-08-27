ae1f61e37c7ebfb4577c8a67cad3e8fdbbb50eb8
2Tucho <albergromero@gmail.com>
2025-08-27 11:10:06 +0200

    Added exercise 5.18

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -2,19 +2,49 @@ const { test, expect, beforeEach, describe } = require("@playwright/test")
 
 const user = {
     username: "PPaco",
-    name: "Pacp Paquez",
+    name: "Paco Paquez",
     password: "paquitronez"
 }
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:5173")
-  })
+    beforeEach(async ({ page, request }) => {
+        await request.post('http:localhost:3003/api/testing/reset')
+        await request.post('http://localhost:3003/api/users', {
+            data: {
+                name: 'Matti Luukkainen',
+                username: 'mluukkai',
+                password: 'salainen'
+            }
+        })
 
-  test("Login form is shown", async ({ page }) => {
-    await page.getByRole("button", { name: "log in" }).click()
-    await expect(page.getByText("Username")).toBeVisible()
-    await expect(page.getByText("Password")).toBeVisible()
-    await expect(page.getByRole("button", { name: "Log in" })).toBeVisible()
-  })
+        await page.goto("http://localhost:5173")
+    })
+
+    test("Login form is shown", async ({ page }) => {
+        await page.getByRole("button", { name: "log in" }).click()
+        await expect(page.getByText("Username")).toBeVisible()
+        await expect(page.getByText("Password")).toBeVisible()
+        await expect(page.getByRole("button", { name: "Log in" })).toBeVisible()
+    })
+
+    describe('Login', () => {
+        beforeEach(async ({ page, request }) => {
+            await page.getByRole('button', { name: 'log in' }).click()
+        })
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.locator('#username').fill('mluukkai')
+            await page.locator('#password').fill('salainen')
+            await page.getByRole('button', { name: 'Log in' }).click()
+
+            await expect(page.getByText("Matti Luukkainen logged in")).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.locator('#username').fill('mluukkai')
+            await page.locator('#password').fill('wrong')
+            await page.getByRole('button', { name: 'Log in' }).click()
+
+            await expect(page.getByText('Wrong credentials')).toBeVisible()
+        })
+    })
 })
